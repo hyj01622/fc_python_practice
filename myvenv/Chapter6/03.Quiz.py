eae6a479cafe323f8e49e183737398221d7d7598
Yeojin <hyj01622@naver.com>
2022-02-26 00:16:40 +0900

    add : Chapter6 quiz2

---
+++ myvenv/Chapter6/03.Quiz.py
@@ -0,0 +1,29 @@
+# 턱걸이 횟수를 입력받기
+# 1일차 20
+# 2일차 23
+# 3일차 16
+# 4일차 14
+# 5일차 24
+# 6일차 27
+# 7일차 30
+
+count = []
+
+x = int(input('1일차 턱걸이 횟수 >>>'))
+count.append(x)
+x = int(input('1일차 턱걸이 횟수 >>>'))
+count.append(x)
+x = int(input('1일차 턱걸이 횟수 >>>'))
+count.append(x)
+x = int(input('1일차 턱걸이 횟수 >>>'))
+count.append(x)
+x = int(input('1일차 턱걸이 횟수 >>>'))
+count.append(x)
+x = int(input('1일차 턱걸이 횟수 >>>'))
+count.append(x)
+x = int(input('1일차 턱걸이 횟수 >>>'))
+count.append(x)
+
+print(count)
+
+# 평균 구하기
